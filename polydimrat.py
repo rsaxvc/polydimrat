46978c53ba2d66e732dc08da0ff2d337c8e9f063
Richard Allen <rsaxvc@rsaxvc.net>
2014-07-06 08:13:58 -0500

    implemented

---
+++ polydimrat.py
@@ -0,0 +1,13 @@
+import math
+
+R = .5
+print "R=",R
+print "n,volBall,volCube,volRat"
+for ni in range(0,14):
+	n=float(ni)
+	n1 = math.pow( math.pi, n / 2 )
+	n2 = math.pow( R, n )
+	d = math.gamma( n / 2 + 1 )
+	volBall = n1 * n2 / d
+	volCube = math.pow( 2 * R, n )
+	print n,volBall, volCube, volBall/volCube
